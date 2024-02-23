a8586cac91e1d8d7e6de8adf8278999cd0dc764d
MagretBro <magretbro@gmail.com>
2024-02-23 17:59:37 +0700

    add login

--- app-ang-1902/src/app/app.module.ts
+++ app-ang-1902/src/app/app.module.ts
@@ -10,15 +10,17 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { HttpClientModule } from '@angular/common/http';
-import { TableCompComponent } from './table-comp/table-comp.component';
-import { LoginCompComponent } from './login-comp/login-comp.component';
+import { TableCompComponent } from './components/table-comp/table-comp.component';
+import { LoginCompComponent } from './components/login-comp/login-comp.component';
 import { FormsModule } from '@angular/forms';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableCompComponent,
-    LoginCompComponent
+    LoginCompComponent,
+    MainComponent
   ],
   imports: [
     MatSliderModule,
--- app-ang-1902/src/app/components/table-comp/table-comp.component.ts
+++ app-ang-1902/src/app/components/table-comp/table-comp.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../service/data/data.service';
+import { DataService } from '../../service/data/data.service';
 
 
 @Component({
